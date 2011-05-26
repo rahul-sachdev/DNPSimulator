2bef4effa998da6293f029d53d2708097e0ce902
Chris Verges <chris.verges@gmail.com>
2011-05-25 18:16:16 -0700

    Changed VtoWriter.cpp from using nested namespaces to the 'using' keyword.

--- DNP3/VtoWriter.cpp
+++ DNP3/VtoWriter.cpp
@@ -19,138 +19,137 @@
 
 #include <APL/Util.h>
 
-namespace apl {
-	namespace dnp {
+using apl::CriticalSection;
+using apl::dnp::VtoData;
+using apl::dnp::VtoEvent;
+using apl::dnp::VtoWriter;
 
-		VtoWriter::VtoWriter(size_t aMaxVtoChunks) :
-			mMaxVtoChunks(aMaxVtoChunks)
-		{}
+VtoWriter::VtoWriter(size_t aMaxVtoChunks) :
+	mMaxVtoChunks(aMaxVtoChunks)
+{}
 
-		size_t VtoWriter::Write(const boost::uint8_t* apData,
-		                        size_t aLength,
-		                        boost::uint8_t aChannelId)
-		{
-			/*
-			 * The whole function is thread-safe, from start to finish.
-			 */
-			CriticalSection cs(&mLock);
+size_t VtoWriter::Write(const boost::uint8_t* apData,
+                        size_t aLength,
+                        boost::uint8_t aChannelId)
+{
+	/*
+	 * The whole function is thread-safe, from start to finish.
+	 */
+	CriticalSection cs(&mLock);
 
-			/*
-			 * Only write the maximum amount available or requested.  If the
-			 * requested data size is larger than the available buffer space,
-			 * only send what will fit.
-			 */
-			size_t num = Min<size_t>(this->NumBytesAvailable(), aLength);
+	/*
+	 * Only write the maximum amount available or requested.  If the
+	 * requested data size is larger than the available buffer space,
+	 * only send what will fit.
+	 */
+	size_t num = Min<size_t>(this->NumBytesAvailable(), aLength);
 
-			/*
-			 * Chop up the data into Max(255) segments and add it to the queue.
-			 */
-			this->Commit(apData, num, aChannelId);
+	/*
+	 * Chop up the data into Max(255) segments and add it to the queue.
+	 */
+	this->Commit(apData, num, aChannelId);
 
-			/* Tell any listeners that the queue has new data to be read. */
-			if (num > 0)
-				this->NotifyAll();
+	/* Tell any listeners that the queue has new data to be read. */
+	if (num > 0)
+		this->NotifyAll();
 
-			/* Return the number of bytes from apData that were queued. */
-			return num;
-		}
+	/* Return the number of bytes from apData that were queued. */
+	return num;
+}
 
-		void VtoWriter::_Start()
-		{
-			mLock.Lock();
-		}
+void VtoWriter::_Start()
+{
+	this->mLock.Lock();
+}
 
-		void VtoWriter::_End()
-		{
-			mLock.Unlock();
-		}
+void VtoWriter::_End()
+{
+	this->mLock.Unlock();
+}
 
-		void VtoWriter::Commit(const boost::uint8_t* apData,
-		                       size_t aLength,
-		                       boost::uint8_t aChannelId)
-		{
-			/*
-			 * The data is segmented into N number of chunks, each of MAX_SIZE
-			 * bytes, and 1 chunk containing the remainder of the data that is
-			 * less than MAX_SIZE bytes.  We pre-calculate the chunk size to
-			 * avoid the constant comparison overhead in the loop itself.
-			 */
-			size_t complete = aLength / VtoData::MAX_SIZE;
-			size_t partial = aLength % VtoData::MAX_SIZE;
+void VtoWriter::Commit(const boost::uint8_t* apData,
+                       size_t aLength,
+                       boost::uint8_t aChannelId)
+{
+	/*
+	 * The data is segmented into N number of chunks, each of MAX_SIZE
+	 * bytes, and 1 chunk containing the remainder of the data that is
+	 * less than MAX_SIZE bytes.  We pre-calculate the chunk size to
+	 * avoid the constant comparison overhead in the loop itself.
+	 */
+	size_t complete = aLength / VtoData::MAX_SIZE;
+	size_t partial = aLength % VtoData::MAX_SIZE;
 
-			/* First, write the full-sized blocks */
-			for (size_t i = 0; i < complete; ++i)
-			{
-				QueueVtoObject(apData, VtoData::MAX_SIZE, aChannelId);
-				apData += VtoData::MAX_SIZE;
-			}
-
-			/* Next, write the remaining data at the end of the stream */
-			if (partial > 0)
-				QueueVtoObject(apData, partial, aChannelId);
-		}
-
-		void VtoWriter::QueueVtoObject(const boost::uint8_t* apData,
-		                               size_t aLength,
-		                               boost::uint8_t aChannelId)
-		{
-			/*
-			 * Create a new VtoData instance, set the event data associated
-			 * with it, and then push the object onto the transmission queue.
-			 */
-			VtoData vto(apData, aLength);
-
-			VtoEvent evt(vto, PC_CLASS_1, aChannelId);
-			mQueue.push(evt);
-		}
-
-		bool VtoWriter::Read(VtoEvent& arEvent)
-		{
-			/*
-			 * The whole function is thread-safe, from start to finish.
-			 */
-			CriticalSection cs(&mLock);
-
-			/*
-			 * If there is no data on the queue, return false.  This allows
-			 * the function to be used in the conditional block of a loop
-			 * construct.
-			 */
-			if (this->mQueue.size() == 0)
-			{
-				return false;
-			}
-
-			/*
-			 * The queue has data, so pop off the front item, store it in
-			 * arEvent, and return true.
-			 */
-			arEvent = this->mQueue.front();
-			this->mQueue.pop();
-			return true;
-		}
-
-		size_t VtoWriter::Size()
-		{
-			/*
-			 * The whole function is thread-safe, from start to finish.
-			 */
-			CriticalSection cs(&mLock);
-
-			return mQueue.size();
-		}
-
-		size_t VtoWriter::NumChunksAvailable()
-		{
-			return mMaxVtoChunks - mQueue.size();
-		}
-
-		size_t VtoWriter::NumBytesAvailable()
-		{
-			return this->NumChunksAvailable() * VtoData::MAX_SIZE;
-		}
-
+	/* First, write the full-sized blocks */
+	for (size_t i = 0; i < complete; ++i)
+	{
+		this->QueueVtoObject(apData, VtoData::MAX_SIZE, aChannelId);
+		apData += VtoData::MAX_SIZE;
 	}
+
+	/* Next, write the remaining data at the end of the stream */
+	if (partial > 0)
+		this->QueueVtoObject(apData, partial, aChannelId);
+}
+
+void VtoWriter::QueueVtoObject(const boost::uint8_t* apData,
+                               size_t aLength,
+                               boost::uint8_t aChannelId)
+{
+	/*
+	 * Create a new VtoData instance, set the event data associated
+	 * with it, and then push the object onto the transmission queue.
+	 */
+	VtoData vto(apData, aLength);
+
+	VtoEvent evt(vto, PC_CLASS_1, aChannelId);
+	this->mQueue.push(evt);
+}
+
+bool VtoWriter::Read(VtoEvent& arEvent)
+{
+	/*
+	 * The whole function is thread-safe, from start to finish.
+	 */
+	CriticalSection cs(&mLock);
+
+	/*
+	 * If there is no data on the queue, return false.  This allows
+	 * the function to be used in the conditional block of a loop
+	 * construct.
+	 */
+	if (this->mQueue.size() == 0)
+	{
+		return false;
+	}
+
+	/*
+	 * The queue has data, so pop off the front item, store it in
+	 * arEvent, and return true.
+	 */
+	arEvent = this->mQueue.front();
+	this->mQueue.pop();
+	return true;
+}
+
+size_t VtoWriter::Size()
+{
+	/*
+	 * The whole function is thread-safe, from start to finish.
+	 */
+	CriticalSection cs(&mLock);
+
+	return mQueue.size();
+}
+
+size_t VtoWriter::NumChunksAvailable()
+{
+	return mMaxVtoChunks - mQueue.size();
+}
+
+size_t VtoWriter::NumBytesAvailable()
+{
+	return this->NumChunksAvailable() * VtoData::MAX_SIZE;
 }
 
 /* vim: set ts=4 sw=4: */
