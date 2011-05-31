381bb1bffb70e8f8d7378e2bfb02986422fe1a98
Chris Verges <chris.verges@gmail.com>
2011-05-31 10:51:56 -0700

    Adjusted alignment.

--- DNP3Test/TestSlaveEventBuffer.cpp
+++ DNP3Test/TestSlaveEventBuffer.cpp
@@ -27,97 +27,96 @@ using namespace std;
 using namespace apl;
 using namespace apl::dnp;
 
-
-	BOOST_AUTO_TEST_SUITE(SlaveEventBufferSuite)
-		void PushEvents(SlaveEventBuffer& b, size_t aNumEvent, size_t aNumIndices)
-		{
-			for(size_t i=0; i<aNumEvent; ++i) {
-				b.Update(Analog(i), PC_CLASS_1, i%aNumIndices);
-			}
+BOOST_AUTO_TEST_SUITE(SlaveEventBufferSuite)
+	void PushEvents(SlaveEventBuffer& b, size_t aNumEvent, size_t aNumIndices)
+	{
+		for(size_t i=0; i<aNumEvent; ++i) {
+			b.Update(Analog(i), PC_CLASS_1, i%aNumIndices);
 		}
+	}
 
-		template <class T>
-		void OverflowTest(const T& first, const T& second)
-		{
-			EventMaxConfig cfg(1, 1, 1, 0);
-			SlaveEventBuffer b(cfg);
-			b.Update(first, PC_CLASS_1, 0);
-			BOOST_REQUIRE_FALSE(b.IsOverflow());
-			BOOST_REQUIRE(b.HasEventData());
-			BOOST_REQUIRE_EQUAL(b.NumType(Convert(T::MeasEnum)), 1);
-			b.Update(second, PC_CLASS_1, 1);
-			BOOST_REQUIRE(b.IsOverflow());
-		}
+	template <class T>
+	void OverflowTest(const T& first, const T& second)
+	{
+		EventMaxConfig cfg(1, 1, 1, 0);
+		SlaveEventBuffer b(cfg);
+		b.Update(first, PC_CLASS_1, 0);
+		BOOST_REQUIRE_FALSE(b.IsOverflow());
+		BOOST_REQUIRE(b.HasEventData());
+		BOOST_REQUIRE_EQUAL(b.NumType(Convert(T::MeasEnum)), 1);
+		b.Update(second, PC_CLASS_1, 1);
+		BOOST_REQUIRE(b.IsOverflow());
+	}
 
-		BOOST_AUTO_TEST_CASE(AnalogInsertion)
-		{
-			const size_t NUM_EVENT = 100;
-			const size_t NUM_INDICES = 10;
+	BOOST_AUTO_TEST_CASE(AnalogInsertion)
+	{
+		const size_t NUM_EVENT = 100;
+		const size_t NUM_INDICES = 10;
 
-			EventMaxConfig cfg(0, NUM_INDICES, 0, 0);
+		EventMaxConfig cfg(0, NUM_INDICES, 0, 0);
 
-			SlaveEventBuffer b(cfg);
+		SlaveEventBuffer b(cfg);
 
-			PushEvents(b, NUM_EVENT, NUM_INDICES); //push lots of events but only
-			BOOST_REQUIRE_FALSE(b.IsOverflow());
-			BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), NUM_INDICES);
+		PushEvents(b, NUM_EVENT, NUM_INDICES); //push lots of events but only
+		BOOST_REQUIRE_FALSE(b.IsOverflow());
+		BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), NUM_INDICES);
 
-			b.Select(BT_ANALOG, PC_CLASS_1); //select all the events
-			BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), NUM_INDICES);
-			PushEvents(b, NUM_EVENT, NUM_INDICES);
-			BOOST_REQUIRE_FALSE(b.IsOverflow());
-			BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), 2*NUM_INDICES);
+		b.Select(BT_ANALOG, PC_CLASS_1); //select all the events
+		BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), NUM_INDICES);
+		PushEvents(b, NUM_EVENT, NUM_INDICES);
+		BOOST_REQUIRE_FALSE(b.IsOverflow());
+		BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), 2*NUM_INDICES);
 
-			//now deselect
-			b.Deselect();
-			BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), NUM_INDICES);
-			BOOST_REQUIRE_FALSE(b.IsOverflow()); //still shouldn't be
+		//now deselect
+		b.Deselect();
+		BOOST_REQUIRE_EQUAL(b.NumType(BT_ANALOG), NUM_INDICES);
+		BOOST_REQUIRE_FALSE(b.IsOverflow()); //still shouldn't be
+	}
 
-		}
+	BOOST_AUTO_TEST_CASE(OverflowAnalog)
+	{
+		OverflowTest(Analog(5), Analog(6));
+	}
 
-		BOOST_AUTO_TEST_CASE(OverflowAnalog)
-		{
-			OverflowTest(Analog(5), Analog(6));
-		}
-		BOOST_AUTO_TEST_CASE(OverflowBinary)
-		{
-			OverflowTest(Binary(true), Binary(false));
-		}
-		BOOST_AUTO_TEST_CASE(OverflowCounter)
-		{
-			OverflowTest(Counter(1), Counter(6));
-		}
+	BOOST_AUTO_TEST_CASE(OverflowBinary)
+	{
+		OverflowTest(Binary(true), Binary(false));
+	}
 
-		BOOST_AUTO_TEST_CASE(ClassSelect)
-		{
-			EventMaxConfig cfg(10, 10, 10, 0);
-			SlaveEventBuffer b(cfg);
-			b.Update(Binary(true), PC_CLASS_1, 0);
-			b.Update(Binary(false), PC_CLASS_1, 0);
-			b.Update(Analog(5), PC_CLASS_1, 0);
-			b.Update(Analog(6), PC_CLASS_1, 1);
-			b.Update(Counter(1), PC_CLASS_1, 0);
-			b.Update(Counter(3), PC_CLASS_1, 1);
+	BOOST_AUTO_TEST_CASE(OverflowCounter)
+	{
+		OverflowTest(Counter(1), Counter(6));
+	}
 
-			b.Select(PC_CLASS_1, 1);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 1);
-			b.Deselect();
+	BOOST_AUTO_TEST_CASE(ClassSelect)
+	{
+		EventMaxConfig cfg(10, 10, 10, 0);
+		SlaveEventBuffer b(cfg);
+		b.Update(Binary(true), PC_CLASS_1, 0);
+		b.Update(Binary(false), PC_CLASS_1, 0);
+		b.Update(Analog(5), PC_CLASS_1, 0);
+		b.Update(Analog(6), PC_CLASS_1, 1);
+		b.Update(Counter(1), PC_CLASS_1, 0);
+		b.Update(Counter(3), PC_CLASS_1, 1);
 
-			b.Select(PC_CLASS_1, 3);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 2);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_ANALOG), 1);
-			b.Deselect();
+		b.Select(PC_CLASS_1, 1);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 1);
+		b.Deselect();
 
-			b.Select(PC_CLASS_1, 5);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 2);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_ANALOG), 2);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_COUNTER), 1);
-			b.Deselect();
+		b.Select(PC_CLASS_1, 3);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 2);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_ANALOG), 1);
+		b.Deselect();
 
-			b.Select(PC_CLASS_1, 6);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 2);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_ANALOG), 2);
-			BOOST_REQUIRE_EQUAL(b.NumSelected(BT_COUNTER), 2);
-		}
+		b.Select(PC_CLASS_1, 5);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 2);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_ANALOG), 2);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_COUNTER), 1);
+		b.Deselect();
 
-	BOOST_AUTO_TEST_SUITE_END()
+		b.Select(PC_CLASS_1, 6);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_BINARY), 2);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_ANALOG), 2);
+		BOOST_REQUIRE_EQUAL(b.NumSelected(BT_COUNTER), 2);
+	}
+BOOST_AUTO_TEST_SUITE_END()
