ee3f4a11f6907443876b75b4a02b0238869d7aba
jadamcrain <jadamcrain@gmail.com>
2011-06-13 17:36:32 -0400

    Formatting of TeamCity integration

--- TeamCitySupport/teamcity_messages.h
+++ TeamCitySupport/teamcity_messages.h
@@ -1,11 +1,11 @@
 /* Copyright 2009 JetBrains
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,32 +21,34 @@
 #include <string>
 #include <iostream>
 
-namespace JetBrains {
+namespace JetBrains
+{
 
 bool underTeamcity();
 
-class TeamcityMessages {
-    std::ostream *m_out;
-    
-protected:
-    std::string escape(std::string s);
+class TeamcityMessages
+{
+	std::ostream* m_out;
 
-    void openMsg(const std::string &name);
-    void writeProperty(std::string name, std::string value);
-    void closeMsg();
+protected:
+	std::string escape(std::string s);
+
+	void openMsg(const std::string& name);
+	void writeProperty(std::string name, std::string value);
+	void closeMsg();
 
 public:
-    TeamcityMessages();
-    
-    void setOutput(std::ostream &);
-    
-    void suiteStarted(std::string name);
-    void suiteFinished(std::string name);
-    
-    void testStarted(std::string name);
-    void testFailed(std::string name, std::string message, std::string details = "");
-    void testIgnored(std::string name, std::string message = "");
-    void testFinished(std::string name);    
+	TeamcityMessages();
+
+	void setOutput(std::ostream&);
+
+	void suiteStarted(std::string name);
+	void suiteFinished(std::string name);
+
+	void testStarted(std::string name);
+	void testFailed(std::string name, std::string message, std::string details = "");
+	void testIgnored(std::string name, std::string message = "");
+	void testFinished(std::string name);
 };
 
 }
