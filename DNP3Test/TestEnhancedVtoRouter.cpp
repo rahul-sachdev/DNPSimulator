8539b1705d2632d556b01f0e79d447d683ad612a
jadamcrain <jadamcrain@gmail.com>
2011-06-30 14:34:20 -0400

    Additional test case for the connection handling

--- DNP3Test/TestEnhancedVtoRouter.cpp
+++ DNP3Test/TestEnhancedVtoRouter.cpp
@@ -22,6 +22,7 @@
 #include <APL/Log.h>
 #include <APL/ToHex.h>
 
+#include <APLTestTools/BufferHelpers.h>
 #include <APLTestTools/LogTester.h>
 #include <APLTestTools/MockPhysicalLayerAsync.h>
 #include <APLTestTools/MockTimerSource.h>
@@ -42,8 +43,8 @@ public:
 	VtoRouterTestClassBase(size_t aWriterSize) :
 		LogTester(false),
 		phys(mLog.GetLogger(LEV_DEBUG, "phys")),
-		writer(mLog.GetLogger(LEV_DEBUG, "writer"), aWriterSize),
-		mts()
+		writer(mLog.GetLogger(LEV_DEBUG, "writer"), aWriterSize),		
+		pRouter(NULL)
 	{}
 
 	void Update(const VtoData& arEvent, PointClass aClass, size_t aIndex) {
@@ -63,11 +64,39 @@ public:
 		else return false;
 	}
 
+	void CheckLocalChannelConnectedMessage(bool connected) {
+		BOOST_REQUIRE(writer.Size() > 0);
+		VtoEvent vto;
+		BOOST_REQUIRE(Read(vto));
+		BOOST_REQUIRE_EQUAL(vto.mIndex, 255);
+		BOOST_REQUIRE_EQUAL(vto.mValue.mpData[0], 88);
+		BOOST_REQUIRE_EQUAL(vto.mValue.mpData[1], (connected ? 0 : 1));
+	}
+
+	void UpdateVtoData(const std::string& arHex) {
+		HexSequence hs(arHex);
+		VtoData data(hs.Buffer(), hs.Size());
+		pRouter->OnVtoDataReceived(data);
+	}
+
+	void SetRemoteState(bool online) {
+		VtoData data(online ? VTODT_REMOTE_OPENED : VTODT_REMOTE_CLOSED);
+		pRouter->OnVtoDataReceived(data);
+	}
+
+	void CheckVtoData(const std::string& arData) {
+		VtoEvent vto;
+		BOOST_REQUIRE(Read(vto));
+		BOOST_REQUIRE_EQUAL(88, vto.mIndex); // the channel id
+		const std::string hex = toHex(vto.mValue.mpData, vto.mValue.GetSize(), true);
+		BOOST_REQUIRE_EQUAL(arData, hex);
+	}
+
 	MockPhysicalLayerAsync phys;
 	VtoWriter writer;
 	MockTimerSource mts;
-
 	std::queue<VtoEvent> mQueue;
+	EnhancedVtoRouter* pRouter;
 };
 
 class ServerVtoRouterTestClass : public VtoRouterTestClassBase
@@ -76,20 +105,8 @@ public:
 	ServerVtoRouterTestClass(const VtoRouterSettings& arSettings = VtoRouterSettings(88, true, true), size_t aWriterSize = 100) :
 		VtoRouterTestClassBase(aWriterSize),
 		router(arSettings, mLog.GetLogger(LEV_DEBUG, "router"), &writer, &phys, &mts) {
+		pRouter = &router;
 		writer.AddVtoCallback(&router);
-	}
-
-	void CheckLocalChannelConnectedMessage(bool connected) {
-		BOOST_REQUIRE(writer.Size() > 0);
-		VtoEvent vto;
-		BOOST_REQUIRE(Read(vto));
-		BOOST_REQUIRE_EQUAL(vto.mValue.mpData[0], 88);
-		BOOST_REQUIRE_EQUAL(vto.mValue.mpData[1], (connected ? 0 : 1));
-	}
-
-	void SetRemoteState(bool online) {
-		VtoData data(online ? VTODT_REMOTE_OPENED : VTODT_REMOTE_CLOSED);
-		router.OnVtoDataReceived(data);
 	}
 
 	ServerSocketVtoRouter router;
@@ -101,31 +118,10 @@ public:
 	ClientVtoRouterTestClass(const VtoRouterSettings& arSettings = VtoRouterSettings(88, true, true), size_t aWriterSize = 100) :
 		VtoRouterTestClassBase(aWriterSize),
 		router(arSettings, mLog.GetLogger(LEV_DEBUG, "router"), &writer, &phys, &mts) {
+		pRouter = &router;
 		writer.AddVtoCallback(&router);
 	}
-
-	void CheckLocalChannelConnectedMessage(bool connected) {
-		BOOST_REQUIRE(writer.Size() > 0);
-		VtoEvent vto;
-		BOOST_REQUIRE(Read(vto));
-		BOOST_REQUIRE_EQUAL(vto.mIndex, 255);
-		BOOST_REQUIRE_EQUAL(vto.mValue.mpData[0], 88);
-		BOOST_REQUIRE_EQUAL(vto.mValue.mpData[1], (connected ? 0 : 1));
-	}
-	void SetRemoteState(bool online) {
-		VtoData data(online ? VTODT_REMOTE_OPENED : VTODT_REMOTE_CLOSED);
-		router.OnVtoDataReceived(data);
-	}
-	void CheckVtoData(const std::string& arData) {
-		VtoEvent vto;
-		BOOST_REQUIRE(Read(vto));
-		BOOST_REQUIRE_EQUAL(88, vto.mIndex); // the channel id
-
-		const std::string hex = toHex(vto.mValue.mpData, vto.mValue.GetSize(), true);
-
-		BOOST_REQUIRE_EQUAL(arData, hex);
-	}
-
+			
 	ClientSocketVtoRouter router;
 };
 
@@ -161,23 +157,41 @@ BOOST_AUTO_TEST_CASE(ServerSendsMagicChannelLocalConnected)
 	rtc.CheckLocalChannelConnectedMessage(false);
 }
 
+BOOST_AUTO_TEST_CASE(ServerReceivingDataWhenRemoteIsClosedCausesNotification)
+{
+	ServerVtoRouterTestClass rtc;
+
+	rtc.UpdateVtoData("01 02 03");
+	rtc.CheckLocalChannelConnectedMessage(false);
+}
+
+void TestDuplicateRemoteOpenCausesLocalReconnect(VtoRouterTestClassBase& arTest)
+{
+	arTest.SetRemoteState(true);
+
+	arTest.mts.Dispatch();
+	BOOST_REQUIRE(arTest.phys.IsOpening());
+	arTest.phys.SignalOpenSuccess();
+	arTest.mts.Dispatch();
+
+	arTest.CheckLocalChannelConnectedMessage(true);
+
+	arTest.SetRemoteState(true);
+	BOOST_REQUIRE(arTest.phys.IsClosing());
+	arTest.phys.TriggerClose();	
+	arTest.CheckLocalChannelConnectedMessage(false);
+}
+
 BOOST_AUTO_TEST_CASE(ServerDuplicateRemoteOpenCausesLocalReconnect)
 {
+	ServerVtoRouterTestClass rtc;
+	TestDuplicateRemoteOpenCausesLocalReconnect(rtc);
+}
+
+BOOST_AUTO_TEST_CASE(ClientDuplicateRemoteOpenCausesLocalReconnect)
+{
 	ClientVtoRouterTestClass rtc;
-
-	rtc.SetRemoteState(true);
-
-	rtc.mts.Dispatch();
-	BOOST_REQUIRE(rtc.phys.IsOpening());
-	rtc.phys.SignalOpenSuccess();
-	rtc.mts.Dispatch();
-
-	rtc.CheckLocalChannelConnectedMessage(true);
-
-	rtc.SetRemoteState(true);
-	BOOST_REQUIRE(rtc.phys.IsClosing());
-	rtc.phys.TriggerClose();	
-	rtc.CheckLocalChannelConnectedMessage(false);
+	TestDuplicateRemoteOpenCausesLocalReconnect(rtc);
 }
 
 BOOST_AUTO_TEST_CASE(ClientStartsOpeningAfterRemoteConnection)
@@ -223,25 +237,6 @@ BOOST_AUTO_TEST_CASE(ClientSendsMagicChannelLocalConnected)
 
 	rtc.CheckVtoData("01 02 03 04 05");
 	rtc.CheckVtoData("06 07 08 09 0A");
-	rtc.CheckLocalChannelConnectedMessage(false);
-}
-
-BOOST_AUTO_TEST_CASE(ClientDuplicateRemoteOpenCausesLocalReconnect)
-{
-	ClientVtoRouterTestClass rtc;
-
-	rtc.SetRemoteState(true);
-
-	rtc.mts.Dispatch();
-	BOOST_REQUIRE(rtc.phys.IsOpening());
-	rtc.phys.SignalOpenSuccess();
-	rtc.mts.Dispatch();
-
-	rtc.CheckLocalChannelConnectedMessage(true);
-
-	rtc.SetRemoteState(true);
-	BOOST_REQUIRE(rtc.phys.IsClosing());
-	rtc.phys.TriggerClose();	
 	rtc.CheckLocalChannelConnectedMessage(false);
 }
 
