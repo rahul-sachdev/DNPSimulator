25318f243f7e469edc896012f62a97bd491dc699
jadamcrain <jadamcrain@gmail.com>
2012-05-23 13:46:38 -0400

    Started working on a DotNetMasterDemo, but I think I am running into x64/x32 issues

---
+++ DotNetMasterDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DNPDotNet;
+
+namespace DotNetMasterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryOutputDN bo = new BinaryOutputDN(ControlCodeDN.CC_LATCH_ON, 1, 100, 100);
+
+        }
+    }
+}
