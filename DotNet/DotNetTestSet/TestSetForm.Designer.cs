bfaa1ff825df95423d3b88a8d55570c65634d4b0
jadamcrain <jadamcrain@gmail.com>
2012-06-09 12:57:20 -0400

    Basic project settings for a grpahical testset

---
+++ DotNet/DotNetTestSet/TestSetForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DotNetTestSet
+{
+    partial class TestSetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TestSetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 453);
+            this.Name = "TestSetForm";
+            this.Text = "TestSet";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ DotNet/DotNetTestSet/TestSetForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DotNetTestSet
+{
+    public partial class TestSetForm : Form
+    {
+        public TestSetForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
