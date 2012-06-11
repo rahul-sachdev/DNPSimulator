d76e3b25e816733ca07b893f234e27d2ae9d0aca
jadamcrain <jadamcrain@gmail.com>
2012-06-11 12:36:08 -0400

    Moved the TestSetControlLibrary and added the start of a tree control

---
+++ DotNet/TestSetControlLibrary/LogControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using DNP3.Interface;
+
+namespace DotNetTestSet
+{
+    public partial class LogControl : UserControl
+    {
+        public LogControl()
+        {
+            InitializeComponent();
+        }
+
+        public void AddLogEntry(LogEntry le)
+        {
+            this.listViewMessages.Items.Add(new ListViewItem(new String[] { le.filterLevel.ToString(), le.time.ToShortTimeString(), le.message }));            
+        }
+    }
+}
